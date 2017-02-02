eaee87d0c7145c93ef04095cb518f7f3b553e4e7
Alex <amcel1288@gmail.com>
2017-02-02 12:58:57 -0800

    feature

--- src/routes/manage/components/display.js
+++ src/routes/manage/components/display.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-
 class Display extends React.Component {
 	render() {
 			return (
 				<div className="Display">
-
+					<ul>
+						{this.props.results.map((result, index) => (<li>{index}</li>))}
+					</ul>
 				</div>
 		);
 	}
