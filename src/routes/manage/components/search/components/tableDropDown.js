8b6adc5e0258c7d7b3971b8947dc583b5bfe1bc3
Alex <amcel1288@gmail.com>
2017-02-04 09:30:49 -0800

    table

--- src/routes/manage/components/search/components/tableDropDown.js
+++ src/routes/manage/components/search/components/tableDropDown.js
@@ -20,7 +20,7 @@ class TableDropDown extends React.Component {
 
 	render() {
 		return (
-				<select name="selectedTable" value={this.state.selectedTable} onChange={this.onTableSelected}>
+				<select className="ui selection dropdown error" name="selectedTable" value={this.state.selectedTable} onChange={this.onTableSelected}>
 					{
 						this
 							.props
