ff361b233ccd83ee0915a9ba974dc73a4b1c39ba
Cameron Wilby <cameron@origincodeacademy.com>
2017-02-09 12:42:05 -0800

    Add authentication

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './routes/login/Login';
+import { Router, Route, hashHistory } from 'react-router';
+import auth from './AuthService';
 
+import axios from 'axios';
+
+
+axios.interceptors.request.use(function (request) {
+    if(auth.loggedIn()) {
+      request.headers.Authorization = 'JWT ' + auth.getToken();
+    }
+
+    return request;
+  }, function (error) {
+    return Promise.reject(error);
+  });
+
+axios.interceptors.response.use(function (response) {
+    if(response.statusCode === 401) {
+      hashHistory.push('/login');
+    }
+
+    return response;
+  }, function (error) {
+    return Promise.reject(error);
+  });
+
+function gatekeeper(nextState, replace) {
+  if (!auth.loggedIn()) {
+    replace({
+      pathname: '/login'
+    })
+  }
+}
 
 ReactDOM.render(
-  <App txt="Vuori"/>,
+  <Router history={hashHistory}>
+    <Route path="/login" component={Login} />
+    <Route path="/app" component={App} onEnter={gatekeeper} />
+  </Router>,
   document.getElementById('root')
 );
